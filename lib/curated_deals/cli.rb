ef7fd48dbc3e819663d22f62634fd2fa9cec92a0
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-09-12 06:09:13 -0600

    cli planning, category debugging

--- lib/curated_deals/cli.rb
+++ lib/curated_deals/cli.rb
@@ -3,8 +3,16 @@ class CuratedDeals::CLI
 
     def call
       puts "Which category of products would you like to see?"
-
+      Category.get_categories
     end
 
   end
 end
+
+# greet
+# intro
+# menu
+# list categories by index
+# display chosen category
+# list products within chosen category
+# list chosen product
