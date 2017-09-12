14d346f664435673dcd6a1349648c4489ed061f7
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-09-12 10:10:26 -0600

    working on categories

--- lib/curated_deals/category.rb
+++ lib/curated_deals/category.rb
@@ -1,10 +1,11 @@
 module CuratedDeals
   class Category
-    attr_accessor :name, :products
+    attr_accessor :name, :products, :products_list_url
     @@all = []
 
-    def initialize(name)
+    def initialize(name, products_list_url)
       @name = name
+      @products_list_url = products_list_url
       @@all << self
       @products = []
     end
@@ -23,12 +24,17 @@ module CuratedDeals
       categories_page.css(".CollectionGrid-tile").each do |category|
         name = category.css(".CollectionGrid-tileName").text.gsub(/\n/, '')
         products_list_url = "https://canopy.co" + category.attr('href')
-        binding.pry
       end
       end
 
+    def self.create_by_name(name, products_list_url)
+      category = self.new(name)
+      category.save
+      category
+      binding.pry
+    end
 
-    #make a category form category array
+    #make a category
 
     #list_categories by index
 
