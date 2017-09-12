f25c15adc9d9fdbdd453468ee69f49990d1d0af8
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-09-12 06:01:39 -0600

    scraping categories and notes

--- lib/curated_deals/category.rb
+++ lib/curated_deals/category.rb
@@ -19,9 +19,16 @@ class CuratedDeals::Category
   #Get category names and urls
   def self.get_categories
     categories_page = Scraper.new.get_page('https://canopy.co/shop/categories')
-
+    categories_page.css(".CollectionGrid-tile").each do |category|
+      name = category.css(".CollectionGrid-tileName").text
+      products_list_url = category.attr('href')
+    end
   end
 
   #list_categories by index
+
+  #display category name
+
+  #A category has many products
 
 end
--- lib/curated_deals/product.rb
+++ lib/curated_deals/product.rb
@@ -2,6 +2,7 @@ module CuratedDeals
   class Product
     #fetch products
     #products belong to a category
+    #generate products by category
     #display products by category
   end
 end
