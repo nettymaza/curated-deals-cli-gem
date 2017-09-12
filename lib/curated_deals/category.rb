84116408743a9e3e5f3181be3836ed5ad8a488b7
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-09-12 05:50:08 -0600

    scraping categories

--- lib/curated_deals/category.rb
+++ lib/curated_deals/category.rb
@@ -1,14 +1,25 @@
 class CuratedDeals::Category
-  attr_accessor :name
+  attr_accessor :name, :products
   @@all = []
 
   def initialize(name)
     @name = name
     @@all << self
+    @products = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
   end
 
   #Get category names and urls
   def self.get_categories
+    categories_page = Scraper.new.get_page('https://canopy.co/shop/categories')
+
   end
 
   #list_categories by index
--- lib/curated_deals/product.rb
+++ lib/curated_deals/product.rb
@@ -1,4 +1,7 @@
 module CuratedDeals
   class Product
+    #fetch products
+    #products belong to a category
+    #display products by category
   end
 end
