5d0402fed14b3f34372ad0208fca3c834b824490
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-09-12 10:53:00 -0600

    working on cli

--- lib/curated_deals/cli.rb
+++ lib/curated_deals/cli.rb
@@ -3,17 +3,29 @@ module CuratedDeals
   class CLI
 
     def call
+      greet
+      start
+
+
+      # list_categorie
+      # list_items from chosen categories
+      # goodbye
+    end
+
+    def greet
+      puts "Welcome to Curated Deals!"
+      puts "Curated Deals features beautiful and functional products, curated by a community with great taste."
+    end
+
+    def start
       puts "Which category of products would you like to see?"
       Category.get_categories
       Category.list_categories
-      # greet
-      # intro
-      # list_categories
+      input = gets.strip
 
-      # list_items from chosen category
-
-      # goodbye
     end
+
+
 
   end
 end
