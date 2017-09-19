5cd2217c08f2a8b02223e05fb6c135d4ca4feccc
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-09-19 17:51:44 -0600

    Added method to show items greater than a certain price

--- lib/curated_deals/product.rb
+++ lib/curated_deals/product.rb
@@ -1,18 +1,37 @@
 require 'launchy'
 
-module CuratedDeals
-  class Product
+
+  class CuratedDeals::Product
     attr_accessor :name, :price, :category
+
+    @@all = []
 
     def initialize(name, price, category, amazon_url)
       @name = name
       @category = category
       @price = price
       @amazon_url = amazon_url
+      @@all << self
+    end
+
+    def self.all
+      @@all
     end
 
     def buy_on_amazon
       Launchy.open(@amazon_url)
     end
+
+    def self.price_greater_than(number)
+      products = []
+
+      @@all.each do |product|
+        if product.price.gsub(/\D/, '').to_i > number
+          products << product
+        end
+      end
+
+      return products
+    end
+
   end
-end
